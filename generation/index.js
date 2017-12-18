d46968c478e874aee991471d2f5eb92a7f52a7f2
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-12-18 23:32:23 +0100

    allow the downloading of external (espresso) files

--- generation/index.js
+++ generation/index.js
@@ -1,15 +1,34 @@
 #!/usr/bin/env node
 const generateIOSAdapters = require("./adapters/ios");
 const iosFiles = {
-  "../detox/ios/EarlGrey/EarlGrey/Action/GREYActions.h": "../detox/src/ios/earlgreyapi/GREYActions.js",
-  "../detox/ios/Detox/GREYMatchers+Detox.h": "../detox/src/ios/earlgreyapi/GREYMatchers+Detox.js",
-  "../detox/ios/EarlGrey/EarlGrey/Matcher/GREYMatchers.h": "../detox/src/ios/earlgreyapi/GREYMatchers.js",
+	"../detox/ios/EarlGrey/EarlGrey/Action/GREYActions.h":
+		"../detox/src/ios/earlgreyapi/GREYActions.js",
+	"../detox/ios/Detox/GREYMatchers+Detox.h":
+		"../detox/src/ios/earlgreyapi/GREYMatchers+Detox.js",
+	"../detox/ios/EarlGrey/EarlGrey/Matcher/GREYMatchers.h":
+		"../detox/src/ios/earlgreyapi/GREYMatchers.js"
 };
 
 generateIOSAdapters(iosFiles);
+const externalFilesToDownload = {
+	"android.support.test.espresso.action.ViewActions":
+		"../detox/src/android/espressoapi/ViewActions.js"
+};
 
 const generateAndroidAdapters = require("./adapters/android");
+const downloadEspressoFileByClass = require("./utils/downloadEspresso");
+const downloadedAndroidFilesMap = Object.entries(
+	externalFilesToDownload
+).reduce(
+	(obj, [fullyQualifiedClass, dest]) => ({
+		...obj,
+		[downloadEspressoFileByClass(fullyQualifiedClass)]: dest
+	}),
+	{}
+);
 const androidFiles = {
-  "../detox/android/detox/src/main/java/com/wix/detox/espresso/DetoxAction.java": "../detox/src/android/espressoapi/DetoxAction.js"
+	...downloadedAndroidFilesMap,
+	"../detox/android/detox/src/main/java/com/wix/detox/espresso/DetoxAction.java":
+		"../detox/src/android/espressoapi/DetoxAction.js"
 };
-generateAndroidAdapters(androidFiles); 
+generateAndroidAdapters(androidFiles);
