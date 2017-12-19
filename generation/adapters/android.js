78e08b3f66452415a0a17dbb9921557f8a6f3589
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-12-19 14:48:09 +0100

    allow string type and enable type text action

--- generation/adapters/android.js
+++ generation/adapters/android.js
@@ -1,17 +1,18 @@
 const t = require("babel-types");
 const generator = require("../core/generator");
 
-const { isNumber } = require("../core/type-checks");
+const { isNumber, isString } = require("../core/type-checks");
 
 const typeCheckInterfaces = {
 	Integer: isNumber,
-	double: isNumber
+	double: isNumber,
+	String: isString
 };
 
 module.exports = generator({
 	typeCheckInterfaces,
 	supportedContentSanitizersMap: {},
-	supportedTypes: ["Integer", "int", "double"],
+	supportedTypes: ["Integer", "int", "double", "String"],
 	renameTypesMap: {
 		int: "Integer" // TODO: add test
 	},
