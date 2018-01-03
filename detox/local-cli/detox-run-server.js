776d504f3d7c7d141cb36ffe1feae791cc900c0f
Igor Borges <igor@borges.me>
2018-01-03 18:50:02 -0200

    revert detox run-server standalone arguments

--- detox/local-cli/detox-run-server.js
+++ detox/local-cli/detox-run-server.js
@@ -5,20 +5,11 @@ const cp = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-program
-  .option('-s, --server [host]',
-    `The IP to bind detox-server`)
-  .option('-p, --port [value]',
-    'The port to bind detox-server')
-  .parse(process.argv);
-
-const server = program.server ? `--server ${program.server}` : '';
-const port = program.port ? `--port ${program.port}` : '';
-const arguments = `${server} ${port}`;
+program.parse(process.argv);
 
 if (fs.existsSync(path.join(process.cwd(), 'node_modules/.bin/detox-server'))) {
-  cp.execSync(`node_modules/.bin/detox-server ${arguments}`, {stdio: 'inherit'});
+  cp.execSync('node_modules/.bin/detox-server', {stdio: 'inherit'});
 } else {
-  cp.execSync(`node_modules/detox/node_modules/.bin/detox-server ${arguments}`, {stdio: 'inherit'});
+  cp.execSync('node_modules/detox/node_modules/.bin/detox-server', {stdio: 'inherit'});
 }
 
