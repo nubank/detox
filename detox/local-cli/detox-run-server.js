b8892b4b397cecb8adb076a5be199a4a85205fdf
Igor Borges <igor@borges.me>
2018-01-03 17:44:33 -0200

    let detox-server set custom host and port on cli

--- detox/local-cli/detox-run-server.js
+++ detox/local-cli/detox-run-server.js
@@ -5,11 +5,20 @@ const cp = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-program.parse(process.argv);
+program
+  .option('-s, --server [host]',
+    `The IP to bind detox-server`)
+  .option('-p, --port [value]',
+    'The port to bind detox-server')
+  .parse(process.argv);
+
+const server = program.server ? `--server ${program.server}` : '';
+const port = program.port ? `--port ${program.port}` : '';
+const arguments = `${server} ${port}`;
 
 if (fs.existsSync(path.join(process.cwd(), 'node_modules/.bin/detox-server'))) {
-  cp.execSync('node_modules/.bin/detox-server', {stdio: 'inherit'});
+  cp.execSync(`node_modules/.bin/detox-server ${arguments}`, {stdio: 'inherit'});
 } else {
-  cp.execSync('node_modules/detox/node_modules/.bin/detox-server', {stdio: 'inherit'});
+  cp.execSync(`node_modules/detox/node_modules/.bin/detox-server ${arguments}`, {stdio: 'inherit'});
 }
 
